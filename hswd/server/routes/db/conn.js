de074fccac2f7c6f0411b49639a06cd54c75a601
harsh <109368583+harshsharma1981@users.noreply.github.com>
2024-06-23 12:50:37 +0530

    Delete hswd/server directory

--- hswd/server/routes/db/conn.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-mongoose.connect(`${process.env.MONURL}`, {
-
-  // useCreateIndex: true // Use this to avoid deprecation warning for ensureIndex
-}).then(()=>{console.log("successful")}).catch((e)=>{
-  console.log(e);
-})
